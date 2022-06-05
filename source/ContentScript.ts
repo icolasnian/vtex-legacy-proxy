698fdefb4140f862c4ea3968723b60fec7495045
nicolasian <80378569+icolasnian@users.noreply.github.com>
2022-06-05 03:43:43 -0300

    remove sender param

--- source/ContentScript.ts
+++ source/ContentScript.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable func-names */
 
-chrome.runtime.onMessage.addListener(function (request, sender) {
+chrome.runtime.onMessage.addListener(function (request) {
   console.log(
     `%cVTEX Legacy Proxy %c| Rule applied: %c${request.name}`,
     'color: #e31c58;',
